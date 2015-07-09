9bb0d1899052b13c0e73939ced98fd523bf1ea64
Dani Artavia <daniartavia.29@gmail.com>
2015-07-08 18:17:01 -0600

    Create desinstalador.sh

---
+++ desinstalador.sh
@@ -0,0 +1,7 @@
+#menu-cierre
+echo "Eliminando menu-cierre"
+echo "Eliminando iconos"
+sudo rm -rf /usr/share/menu-cierre
+echo "Eliminando el binario de /usr/bin"
+sudo rm /usr/bin/menu-cierre
+echo "menu-cierre ha sido desinstalado correctamente"
